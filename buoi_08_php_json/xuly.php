3041020120452bae99cb821bbbe98db169423d67
soiqualang <soiqualang.chentreu@gmail.com>
2020-04-22 20:55:19 +0700

    buoi 8

--- buoi_08_php_json/xuly.php
+++ buoi_08_php_json/xuly.php
@@ -5,7 +5,7 @@
     define("PG_HOST","localhost");
     define("PG_USER","postgres");
     define("PG_PORT","5433");
-    define("PG_PASS","2679191");
+    define("PG_PASS","*********");
     $dbcon=pg_connect("dbname=".PG_DB." user=".PG_USER." password=".PG_PASS." host=".PG_HOST." port=".PG_PORT);
 
 
